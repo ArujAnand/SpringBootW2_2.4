74193fd519245ff358007ceef90e03df52a104de
Aruj Anand <arujanand2@gmail.com>
2025-06-09 20:49:27 +0530

    Chapter 2.1

---
+++ springbootwebtutorial/src/test/java/codingshuttle/springbootwebtutorial/springbootwebtutorial/SpringBootWebTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package codingshuttle.springbootwebtutorial.springbootwebtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
