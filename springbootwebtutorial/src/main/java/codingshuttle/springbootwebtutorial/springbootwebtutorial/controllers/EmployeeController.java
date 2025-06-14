ab0b594e1c64b93779a2c248c182fce5c674ea1b
Aruj Anand <arujanand2@gmail.com>
2025-06-14 12:24:14 +0530

    MVC architecture with controllers not dealing with repo logic and services b/w repo and controller when dealing with DTOs, using response entities as recommneded with REST

--- springbootwebtutorial/src/main/java/codingshuttle/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
+++ springbootwebtutorial/src/main/java/codingshuttle/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
@@ -2,6 +2,8 @@ package codingshuttle.springbootwebtutorial.springbootwebtutorial.controllers;
 
 import codingshuttle.springbootwebtutorial.springbootwebtutorial.dto.EmployeeDTO;
 import codingshuttle.springbootwebtutorial.springbootwebtutorial.services.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,27 +34,35 @@ public class EmployeeController {
     }
 
     @GetMapping
-    public List<EmployeeDTO> getAllEmployees(@RequestParam(required = false) Integer age, @RequestParam(required = false) String sortBy) {
-        return employeeService.getAllEmployees();
+    public ResponseEntity<List<EmployeeDTO>> getAllEmployees(@RequestParam(required = false) Integer age, @RequestParam(required = false) String sortBy) {
+        return ResponseEntity.ok(employeeService.getAllEmployees());
     }
 
     @PutMapping(path = "/{employeeId}")
-    public EmployeeDTO updateEmployeeById(@RequestBody EmployeeDTO employeeDTO, @PathVariable Long employeeId) {
-        return employeeService.updateEmployeeById(employeeDTO, employeeId);
+    public ResponseEntity <EmployeeDTO> updateEmployeeById(@RequestBody EmployeeDTO employeeDTO, @PathVariable Long employeeId) {
+        return ResponseEntity.ok(employeeService.updateEmployeeById(employeeDTO, employeeId));
     }
 
     @PostMapping (path = "/addEmployee")
-    public EmployeeDTO createNewEmployee(@RequestBody EmployeeDTO inputEmployee) {
-        return employeeService.createNewEmployee(inputEmployee);
+    public ResponseEntity <EmployeeDTO> createNewEmployee(@RequestBody EmployeeDTO inputEmployee) {
+        EmployeeDTO savedEmployee = employeeService.createNewEmployee(inputEmployee);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
 
     @DeleteMapping (path = "/{employeeId}")
-    public boolean deleteEmployeeById(@PathVariable Long employeeId) {
-        return employeeService.deleteEmployeeById(employeeId);
+    public ResponseEntity <Boolean> deleteEmployeeById(@PathVariable Long employeeId) {
+        boolean gotDeleted = employeeService.deleteEmployeeById(employeeId);
+
+        if (gotDeleted)
+            return ResponseEntity.ok(true);
+        return ResponseEntity.notFound().build();
     }
 
     @PatchMapping(path = "/{employeeId}")
-    public EmployeeDTO updatePartialEmployeeById (@RequestBody Map<String, Object> updates, @PathVariable Long employeeId) {
-        return employeeService.updatePartialEmployeeById(employeeId, updates);
+    public ResponseEntity <EmployeeDTO> updatePartialEmployeeById (@RequestBody Map<String, Object> updates, @PathVariable Long employeeId) {
+        EmployeeDTO employeeDTO = employeeService.updatePartialEmployeeById(employeeId, updates);
+        if (employeeDTO == null)
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(employeeDTO);
     }
 }
